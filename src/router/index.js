c782b5d3a2a13c41a24ae516cbc91b6b71839e49
D.Boy.Blade <ykw0104@163.com>
2021-03-23 10:31:28 +0800

    P151 import TabBar and configure base information in router index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,39 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+const Home = () => import("views/home/Home")
+const Profile = () => import('views/profile/Profile')
+const Cart = () => import('views/cart/Cart')
+const Category = () => import('views/category/Category')
+
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/profile',
+    component: Profile
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/category',
+    component: Category
+  }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes
 })
 
