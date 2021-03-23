391fc54dda4aaa5b77ae219377f1fd751f91c734
D.Boy.Blade <ykw0104@163.com>
2021-03-24 07:24:53 +0800

    P158 Finished Home.vue > FeatureView.vue

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="home">
-    <!-- 顶部的导航栏 -->
+    <!-- 1. 顶部的导航栏 -->
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
 
-    <!-- 轮播图 -->
+    <!-- 2. 轮播图 -->
     <home-swiper :banners="banners"></home-swiper>
 
-    <!-- 推荐 -->
+    <!-- 3. 推荐 -->
     <recommend-view :recommends="recommends"></recommend-view>
+
+    <!-- 4. 本周流行,就一张图片 -->
+    <feature-view></feature-view>
   </div>
 </template>
 
@@ -17,6 +20,7 @@
 import NavBar from "components/common/navbar/NavBar.vue";
 import HomeSwiper from "views/home/childComps/HomeSwiper.vue";
 import RecommendView from "views/home/childComps/RecommendView";
+import FeatureView from "views/home/childComps/FeatureView";
 
 import { getHomeMultidata } from "network/home.js";
 
@@ -26,6 +30,7 @@ export default {
     NavBar,
     HomeSwiper,
     RecommendView,
+    FeatureView,
   },
   data() {
     return {
@@ -43,8 +48,18 @@ export default {
 };
 </script>
 <style>
+#home {
+  padding-top: 44px;
+}
+
 .home-nav {
   background: var(--color-tint);
   color: #fff;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
 }
 </style>
